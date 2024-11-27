37ee13273a956044505f86e735d338f49884c707
Crowdbotics <team@crowdbotics.com>
2024-11-27 21:33:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aurea-50162",
+  url: "https://aurea-50162.botics.co",
+  api: "https://aurea-50162.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
